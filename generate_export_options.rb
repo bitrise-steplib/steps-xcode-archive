32062e2a1497195f2364c1493d69288ed2b6f778
Krisztian Godrei <godrekrisztian@gmil.com>
2015-10-26 15:37:01 +0000

    merge

---
+++ generate_export_options.rb
@@ -0,0 +1,100 @@
+require 'optparse'
+require 'plist'
+require 'json'
+
+# -----------------------
+# --- functions
+# -----------------------
+
+def fail_with_message(message)
+  `envman add --key BITRISE_XAMARIN_TEST_RESULT --value failed`
+
+  puts "\e[31m#{message}\e[0m"
+  exit(1)
+end
+
+def collect_provision_info(archive_path)
+  applications_path = File.join(archive_path, '/Products/Applications')
+  mobileprovision_path = Dir[File.join(applications_path, '*.app/embedded.mobileprovision')].first
+
+  fail_with_message('No mobileprovision_path found') if mobileprovision_path.nil?
+
+  content = {}
+  plist = Plist.parse_xml(`security cms -D -i #{mobileprovision_path}`)
+
+  plist.each do |key, value|
+    next if key == 'DeveloperCertificates'
+
+    parse_value = nil
+    case value
+    when Hash
+      parse_value = value
+    when Array
+      parse_value = value
+    else
+      parse_value = value.to_s
+    end
+
+    content[key] = parse_value
+  end
+
+  content
+end
+
+def export_method(mobileprovision_content)
+  # if ProvisionedDevices: !nil -> Development
+  # if ProvisionedDevices: nil & "ProvisionsAllDevices": "true" -> Enterprise
+  # if ProvisionedDevices: nil & ProvisionsAllDevices: nil -> AppStore
+  if mobileprovision_content['ProvisionedDevices'].nil?
+    return 'enterprise' if !mobileprovision_content['ProvisionsAllDevices'].nil? && (mobileprovision_content['ProvisionsAllDevices'] == true || mobileprovision_content['ProvisionsAllDevices'] == 'true')
+    return 'app-store'
+  end
+  'development'
+end
+
+# -----------------------
+# --- main
+# -----------------------
+
+puts
+
+# Input validation
+options = {
+  export_options_path: nil,
+  archive_path: nil
+}
+
+parser = OptionParser.new do|opts|
+  opts.banner = 'Usage: step.rb [options]'
+  opts.on('-o', '--export_options_path path', 'Export options path') { |o| options[:export_options_path] = o unless o.to_s == '' }
+  opts.on('-a', '--archive_path path', 'Archive path') { |a| options[:archive_path] = a unless a.to_s == '' }
+  opts.on('-h', '--help', 'Displays Help') do
+    puts opts
+    exit
+  end
+end
+parser.parse!
+
+fail_with_message('export_options_path not specified') unless options[:export_options_path]
+puts "(i) export_options_path: #{options[:export_options_path]}"
+
+fail_with_message('archive_path not specified') unless options[:archive_path]
+puts "(i) archive_path: #{options[:archive_path]}"
+
+puts
+puts '==> Collect infos from mobileprovision'
+
+mobileprovision_content = collect_provision_info(options[:archive_path])
+# team_id = mobileprovision_content['TeamIdentifier'].first
+method = export_method(mobileprovision_content)
+
+puts
+puts '==> Create export options'
+
+export_options = {}
+# export_options[:teamID] = team_id unless team_id.nil?
+export_options[:method] = method unless method.nil?
+
+puts
+puts export_options
+export_options.save_plist(options[:export_options_path])
