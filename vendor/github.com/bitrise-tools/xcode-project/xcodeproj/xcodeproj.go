609ba6258ca30edfb8a583c673d7d2a4b7db7148
Akos Birmacher <akos.birmacher@bitrise.io>
2019-02-04 14:48:37 +0100

    normalize the scheme's name before comparing it with the input

--- vendor/github.com/bitrise-tools/xcode-project/xcodeproj/xcodeproj.go
+++ vendor/github.com/bitrise-tools/xcode-project/xcodeproj/xcodeproj.go
@@ -12,6 +12,7 @@ import (
 	"github.com/bitrise-tools/xcode-project/serialized"
 	"github.com/bitrise-tools/xcode-project/xcodebuild"
 	"github.com/bitrise-tools/xcode-project/xcscheme"
+	"golang.org/x/text/unicode/norm"
 	"howett.net/plist"
 )
 
@@ -189,8 +190,10 @@ func (p XcodeProj) Scheme(name string) (xcscheme.Scheme, bool) {
 		return xcscheme.Scheme{}, false
 	}
 
+	normName := norm.NFC.String(name)
 	for _, scheme := range schemes {
-		if scheme.Name == name {
+		normSchemeName := norm.NFC.String(scheme.Name)
+		if normSchemeName == normName {
 			return scheme, true
 		}
 	}
