1cbbc7dd297eac320af2ec023345eeeea6eb52d4
Richard Bogdan <richard.bogdan@bitrise.io>
2019-09-25 08:45:34 +0200

    TOOL-20 Minor fixes.

--- utils/entitlements.go
+++ utils/entitlements.go
@@ -5,16 +5,16 @@ import (
 	"path/filepath"
 
 	"github.com/bitrise-io/go-xcode/plistutil"
+	"github.com/bitrise-io/xcode-project"
 	"github.com/bitrise-io/xcode-project/serialized"
 	"github.com/bitrise-io/xcode-project/xcodeproj"
-	"github.com/bitrise-io/xcode-project"
 )
 
 // ProjectEntitlementsByBundleID ...
 func ProjectEntitlementsByBundleID(pth, schemeName, configurationName string) (map[string]plistutil.PlistData, error) {
-	scheme,schemeContainerDir,err := project.Scheme(pth,schemeName)
+	scheme, schemeContainerDir, err := project.Scheme(pth, schemeName)
 	if err != nil {
-		return nil, fmt.Errorf("could not get scheme with name %s from path %s", schemeName,pth)
+		return nil, fmt.Errorf("could not get scheme with name %s from path %s", schemeName, pth)
 	}
 	if configurationName == "" {
 		configurationName = scheme.ArchiveAction.BuildConfiguration
@@ -34,12 +34,12 @@ func ProjectEntitlementsByBundleID(pth, schemeName, configurationName string) (m
 		return nil, err
 	}
 
-	project, err := xcodeproj.Open(projectPth)
+	xcodeProj, err := xcodeproj.Open(projectPth)
 	if err != nil {
 		return nil, err
 	}
 
-	mainTarget, ok := project.Proj.Target(archiveEntry.BuildableReference.BlueprintIdentifier)
+	mainTarget, ok := xcodeProj.Proj.Target(archiveEntry.BuildableReference.BlueprintIdentifier)
 	if !ok {
 		return nil, fmt.Errorf("target not found: %s", archiveEntry.BuildableReference.BlueprintIdentifier)
 	}
@@ -49,14 +49,14 @@ func ProjectEntitlementsByBundleID(pth, schemeName, configurationName string) (m
 	entitlementsByBundleID := map[string]serialized.Object{}
 
 	for _, target := range targets {
-		bundleID, err := project.TargetBundleID(target.Name, configurationName)
+		bundleID, err := xcodeProj.TargetBundleID(target.Name, configurationName)
 		if err != nil {
-			return nil, fmt.Errorf("Failed to get target (%s) bundle id: %s", target.Name, err)
+			return nil, fmt.Errorf("failed to get target (%s) bundle id: %s", target.Name, err)
 		}
 
-		entitlements, err := project.TargetCodeSignEntitlements(target.Name, configurationName)
+		entitlements, err := xcodeProj.TargetCodeSignEntitlements(target.Name, configurationName)
 		if err != nil && !serialized.IsKeyNotFoundError(err) {
-			return nil, fmt.Errorf("Failed to get target (%s) bundle id: %s", target.Name, err)
+			return nil, fmt.Errorf("failed to get target (%s) bundle id: %s", target.Name, err)
 		}
 
 		entitlementsByBundleID[bundleID] = entitlements
