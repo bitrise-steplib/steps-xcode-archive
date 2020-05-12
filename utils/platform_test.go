76ab8e77ab00525ea143720ea5c9ade7c0601d56
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-05-12 13:58:45 +0200

    Replace ProjectPlatform with TargetBuildSettingsProvider (#186)
    * Replace ProjectPlatform with TargetBuildSettingsProvider, using xcodebuild showBuildSettings.
    
    * Pass customOptions to xcodeproj.TargetBuildSettings.
    
    * Update assert in tests.
    
    * Go dependencies update.

--- utils/platform_test.go
+++ utils/platform_test.go
@@ -5,77 +5,179 @@ import (
 
 	"github.com/bitrise-io/xcode-project/serialized"
 	"github.com/bitrise-io/xcode-project/xcodeproj"
+	"github.com/bitrise-io/xcode-project/xcscheme"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
-func createProject(configuration string, buildSettings serialized.Object) *xcodeproj.XcodeProj {
-	return &xcodeproj.XcodeProj{
-		Proj: xcodeproj.Proj{
-			BuildConfigurationList: xcodeproj.ConfigurationList{
-				BuildConfigurations: []xcodeproj.BuildConfiguration{
-					xcodeproj.BuildConfiguration{
-						Name:          configuration,
-						BuildSettings: buildSettings,
-					},
-				},
-			},
-		},
-	}
+type MockTargetBuildSettingsProvider struct {
+	mock.Mock
 }
 
-func TestProjectPlatform1(t *testing.T) {
-	proj := createProject("Debug", serialized.Object(map[string]interface{}{"SDKROOT": "iphoneos"}))
-	platform, err := ProjectPlatform(proj, "Debug")
-	require.NoError(t, err)
-	require.Equal(t, iOS, platform)
+// TargetBuildSettings ...
+func (m *MockTargetBuildSettingsProvider) TargetBuildSettings(xcodeProj *xcodeproj.XcodeProj, target, configuration string, customOptions ...string) (serialized.Object, error) {
+	args := m.Called(xcodeProj, target, configuration)
+	return args.Get(0).(serialized.Object), args.Error(1)
 }
 
-func TestProjectPlatform(t *testing.T) {
+func TestBuildableTargetPlatform(t *testing.T) {
 	tests := []struct {
 		name              string
 		xcodeProj         *xcodeproj.XcodeProj
+		scheme            *xcscheme.Scheme
 		configurationName string
+		settings          serialized.Object
 		want              Platform
-		wantErr           string
+		wantErr           bool
 	}{
 		{
-			name:              "Detects iOS platform",
-			xcodeProj:         createProject("Debug", serialized.Object(map[string]interface{}{"SDKROOT": "iphoneos"})),
-			configurationName: "Debug",
+			name: "SDKROOT build settings not defined in the project, but showBuildSettings returns it",
+			xcodeProj: &xcodeproj.XcodeProj{
+				Proj: xcodeproj.Proj{
+					Targets: []xcodeproj.Target{
+						xcodeproj.Target{
+							ID: "target_id",
+						},
+					},
+				},
+			},
+			scheme: &xcscheme.Scheme{
+				BuildAction: xcscheme.BuildAction{
+					BuildActionEntries: []xcscheme.BuildActionEntry{
+						xcscheme.BuildActionEntry{
+							BuildForArchiving: "YES",
+							BuildableReference: xcscheme.BuildableReference{
+								BuildableName:       "bitrise.app",
+								BlueprintIdentifier: "target_id",
+							},
+						},
+					},
+				},
+			},
+			configurationName: "",
+			settings:          serialized.Object(map[string]interface{}{"SDKROOT": "iphoneos"}),
 			want:              iOS,
-			wantErr:           "",
+			wantErr:           false,
 		},
 		{
-			name:              "Fails if configuration not found",
-			xcodeProj:         createProject("Release", serialized.Object(map[string]interface{}{"SDKROOT": "iphoneos"})),
-			configurationName: "Debug",
-			want:              "",
-			wantErr:           "Debug project configuration not found",
-		},
-		{
-			name:              "Fails if SDKROOT not found",
-			xcodeProj:         createProject("Debug", serialized.Object(map[string]interface{}{})),
-			configurationName: "Debug",
-			want:              "",
-			wantErr:           `failed to get SDKROOT: key: string("SDKROOT") not found in: serialized.Object(serialized.Object{})`,
+			name: "fails if showBuildSettings does not return SDKROOT",
+			xcodeProj: &xcodeproj.XcodeProj{
+				Proj: xcodeproj.Proj{
+					Targets: []xcodeproj.Target{
+						xcodeproj.Target{
+							ID: "target_id",
+						},
+					},
+				},
+			},
+			scheme: &xcscheme.Scheme{
+				BuildAction: xcscheme.BuildAction{
+					BuildActionEntries: []xcscheme.BuildActionEntry{
+						xcscheme.BuildActionEntry{
+							BuildForArchiving: "YES",
+							BuildableReference: xcscheme.BuildableReference{
+								BuildableName:       "bitrise.app",
+								BlueprintIdentifier: "target_id",
+							},
+						},
+					},
+				},
+			},
+			configurationName: "",
+			settings:          serialized.Object(map[string]interface{}{}),
+			want:              Platform(""),
+			wantErr:           true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := ProjectPlatform(tt.xcodeProj, tt.configurationName)
-			hasError := err != nil
-			wantError := tt.wantErr != ""
-			if hasError != wantError {
-				t.Errorf("ProjectPlatform() error: %s, want: %s", err, tt.wantErr)
-				return
-			} else if hasError && err.Error() != tt.wantErr {
-				t.Errorf("ProjectPlatform() error: %s, want: %s", err, tt.wantErr)
+			provider := &MockTargetBuildSettingsProvider{}
+			provider.
+				On("TargetBuildSettings", mock.AnythingOfType("*xcodeproj.XcodeProj"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).
+				Return(tt.settings, nil)
+
+			got, err := BuildableTargetPlatform(tt.xcodeProj, tt.scheme, tt.configurationName, provider)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BuildableTargetPlatform() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 
-			if got != tt.want {
-				t.Errorf("ProjectPlatform() = %v, want %v", got, tt.want)
+			provider.AssertExpectations(t)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_getPlatform(t *testing.T) {
+	tests := []struct {
+		name          string
+		buildSettings serialized.Object
+		want          Platform
+		wantErr       bool
+	}{
+		{
+			name:          "iOS",
+			buildSettings: serialized.Object(map[string]interface{}{"SDKROOT": "iphoneos"}),
+			want:          iOS,
+			wantErr:       false,
+		},
+		{
+			name:          "osX",
+			buildSettings: serialized.Object(map[string]interface{}{"SDKROOT": "macosx"}),
+			want:          osX,
+			wantErr:       false,
+		},
+		{
+			name:          "tvOS",
+			buildSettings: serialized.Object(map[string]interface{}{"SDKROOT": "appletvos"}),
+			want:          tvOS,
+			wantErr:       false,
+		},
+		{
+			name:          "watchOS",
+			buildSettings: serialized.Object(map[string]interface{}{"SDKROOT": "watchos"}),
+			want:          watchOS,
+			wantErr:       false,
+		},
+		{
+			name:          "iOS with SDK path",
+			buildSettings: serialized.Object(map[string]interface{}{"SDKROOT": "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS13.4.sdk"}),
+			want:          iOS,
+			wantErr:       false,
+		},
+		{
+			name:          "osX with SDK path",
+			buildSettings: serialized.Object(map[string]interface{}{"SDKROOT": "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk"}),
+			want:          osX,
+			wantErr:       false,
+		},
+		{
+			name:          "tvOS with SDK path",
+			buildSettings: serialized.Object(map[string]interface{}{"SDKROOT": "/Applications/Xcode.app/Contents/Developer/Platforms/AppleTVOS.platform/Developer/SDKs/AppleTVOS.sdk"}),
+			want:          tvOS,
+			wantErr:       false,
+		},
+		{
+			name:          "watchOS with SDK path",
+			buildSettings: serialized.Object(map[string]interface{}{"SDKROOT": "/Applications/Xcode.app/Contents/Developer/Platforms/WatchOS.platform/Developer/SDKs/WatchOS.sdk"}),
+			want:          watchOS,
+			wantErr:       false,
+		},
+		{
+			name:          "unkown SDK path",
+			buildSettings: serialized.Object(map[string]interface{}{"SDKROOT": "/Applications/Xcode.app/Contents/Developer/Platforms/WatchSimulator.platform/Developer/SDKs/WatchSimulator.sdk"}),
+			want:          Platform(""),
+			wantErr:       true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getPlatform(tt.buildSettings)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getPlatform() error = %v, wantErr %v", err, tt.wantErr)
+				return
 			}
+			require.Equal(t, tt.want, got)
 		})
 	}
 }
