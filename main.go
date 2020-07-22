b1a00b6156e2daca85b821f921017f722da13723
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-07-22 15:21:25 +0200

    Make the step failing if scheme selected targeting an app clip target.

--- main.go
+++ main.go
@@ -338,6 +338,18 @@ func main() {
 		fail("Failed to read project platform: %s: %s", absProjectPath, err)
 	}
 
+	mainTarget, err := archivableApplicationTarget(xcodeProj, scheme, configuration)
+	if err != nil {
+		fail("Failed to read main application target: %s", absProjectPath, err)
+	}
+	if mainTarget.ProductType == appClipProductType {
+		log.Errorf("Selected scheme: '%s' targets and App Clip target (%s),", cfg.Scheme, mainTarget.Name)
+		log.Errorf("'Xcode Archive & Export for iOS' step is intended to archive the project using a scheme targeting an Application target.")
+		log.Errorf("Please select a scheme targeting an Application target to archive and export the main Application")
+		log.Errorf("and use 'Export iOS and tvOS Xcode archive' step to export an App Clip.")
+		os.Exit(1)
+	}
+
 	//
 	// Create the Archive with Xcode Command Line tools
 	log.Infof("Create the Archive ...")
