99b9dfa5859de7cf8120502fc961bf95394a1949
Istvan Kovacs <71431677+istvankovacs-bitrise@users.noreply.github.com>
2020-12-15 11:25:02 +0100

    Update fail error message
    Co-authored-by: lpusok <7979773+lpusok@users.noreply.github.com>

--- main.go
+++ main.go
@@ -714,7 +714,7 @@ is available in the $BITRISE_IDEDISTRIBUTION_LOGS_PATH environment variable`)
 
 		if len(appDSYM) > 0 {
 			if err := exportDSYMs(dsymDir, appDSYM); err != nil {
-				fail(err.Error())
+				fail("Failed to export DSYMs: %v", err)
 			}
 		} else {
 			log.Warnf("no app dsyms found")
