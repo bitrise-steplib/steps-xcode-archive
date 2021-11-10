02c016bb6237d7d024a99b349f5369f710bdce81
Laszlo Pusok <laszlo.pusok@bitrise.io>
2021-11-10 09:03:23 +0100

    mod update

--- vendor/github.com/bitrise-io/go-xcode/xcodebuild/xcodebuild.go
+++ vendor/github.com/bitrise-io/go-xcode/xcodebuild/xcodebuild.go
@@ -11,3 +11,19 @@ type CommandModel interface {
 	PrintableCmd() string
 	Command(opts *command.Opts) command.Command
 }
+
+// AuthenticationParams ...
+type AuthenticationParams struct {
+	KeyID     string
+	IsssuerID string
+	KeyPath   string
+}
+
+func (a *AuthenticationParams) args() []string {
+	return []string{
+		"-allowProvisioningUpdates",
+		"-authenticationKeyPath", a.KeyPath,
+		"-authenticationKeyID", a.KeyID,
+		"-authenticationKeyIssuerID", a.IsssuerID,
+	}
+}
