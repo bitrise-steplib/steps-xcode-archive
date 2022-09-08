6c2a1c14b2e8d4f0280e7fba0bc78526d51b373b
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2022-09-08 15:34:34 +0200

    Move step logic to the step package

--- step/archive.go
+++ step/archive.go
@@ -1,4 +1,4 @@
-package main
+package step
 
 import (
 	"bytes"
@@ -7,38 +7,38 @@ import (
 	"strings"
 	"time"
 
-	"github.com/bitrise-io/go-utils/colorstring"
-	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-io/go-utils/progress"
+	"github.com/bitrise-io/go-utils/v2/log"
 	"github.com/bitrise-io/go-xcode/xcodebuild"
 	cache "github.com/bitrise-io/go-xcode/xcodecache"
 	"github.com/bitrise-io/go-xcode/xcpretty"
 )
 
-func runArchiveCommandWithRetry(archiveCmd *xcodebuild.CommandBuilder, useXcpretty bool, swiftPackagesPath string) (string, error) {
-	output, err := runArchiveCommand(archiveCmd, useXcpretty)
+func runArchiveCommandWithRetry(archiveCmd *xcodebuild.CommandBuilder, useXcpretty bool, swiftPackagesPath string, logger log.Logger) (string, error) {
+	output, err := runArchiveCommand(archiveCmd, useXcpretty, logger)
 	if err != nil && swiftPackagesPath != "" && strings.Contains(output, cache.SwiftPackagesStateInvalid) {
-		log.Warnf("Archive failed, swift packages cache is in an invalid state, error: %s", err)
-		log.RWarnf("xcode-archive", "swift-packages-cache-invalid", nil, "swift packages cache is in an invalid state")
+		logger.Warnf("Archive failed, swift packages cache is in an invalid state, error: %s", err)
+		// TODO: analytics log
+		//logger.RWarnf("xcode-archive", "swift-packages-cache-invalid", nil, "swift packages cache is in an invalid state")
 		if err := os.RemoveAll(swiftPackagesPath); err != nil {
 			return output, fmt.Errorf("failed to remove invalid Swift package caches, error: %s", err)
 		}
-		return runArchiveCommand(archiveCmd, useXcpretty)
+		return runArchiveCommand(archiveCmd, useXcpretty, logger)
 	}
 	return output, err
 }
 
-func runArchiveCommand(archiveCmd *xcodebuild.CommandBuilder, useXcpretty bool) (string, error) {
+func runArchiveCommand(archiveCmd *xcodebuild.CommandBuilder, useXcpretty bool, logger log.Logger) (string, error) {
 	if useXcpretty {
 		xcprettyCmd := xcpretty.New(archiveCmd)
 
-		logWithTimestamp(colorstring.Green, "$ %s", xcprettyCmd.PrintableCmd())
+		logger.TDonef("$ %s", xcprettyCmd.PrintableCmd())
 		fmt.Println()
 
 		return xcprettyCmd.Run()
 	}
 	// Using xcodebuild
-	logWithTimestamp(colorstring.Green, "$ %s", archiveCmd.PrintableCmd())
+	logger.TDonef("$ %s", archiveCmd.PrintableCmd())
 	fmt.Println()
 
 	var output bytes.Buffer
---
+++ step/utils.go
@@ -0,0 +1,79 @@
+package step
+
+import (
+	"bufio"
+	"fmt"
+	"regexp"
+	"strings"
+
+	v1command "github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/sliceutil"
+	"github.com/bitrise-io/go-utils/v2/log"
+	"github.com/bitrise-io/go-xcode/exportoptions"
+)
+
+func generateAdditionalOptions(platform string, customOptions []string) []string {
+	destination := "generic/platform=" + platform
+	destinationOptions := []string{"-destination", destination}
+
+	var options []string
+	if len(customOptions) != 0 {
+		if !sliceutil.IsStringInSlice("-destination", customOptions) {
+			options = append(options, destinationOptions...)
+		}
+
+		options = append(options, customOptions...)
+	} else {
+		options = append(options, destinationOptions...)
+	}
+
+	return options
+}
+
+func determineExportMethod(desiredExportMethod string, archiveExportMethod exportoptions.Method, logger log.Logger) (exportoptions.Method, error) {
+	if desiredExportMethod == "auto-detect" {
+		logger.Printf("auto-detect export method specified: using the archive profile's export method: %s", archiveExportMethod)
+		return archiveExportMethod, nil
+	}
+
+	exportMethod, err := exportoptions.ParseMethod(desiredExportMethod)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse export method: %s", err)
+	}
+	logger.Printf("export method specified: %s", desiredExportMethod)
+
+	return exportMethod, nil
+}
+
+func findIDEDistrubutionLogsPath(output string, logger log.Logger) (string, error) {
+	pattern := `IDEDistribution: -\[IDEDistributionLogging _createLoggingBundleAtPath:\]: Created bundle at path '(?P<log_path>.*)'`
+	re := regexp.MustCompile(pattern)
+
+	logger.Printf("Locating IDE distrubution logs path")
+
+	scanner := bufio.NewScanner(strings.NewReader(output))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if match := re.FindStringSubmatch(line); len(match) == 2 {
+			logger.Printf("Located IDE distrubution logs path")
+
+			return match[1], nil
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	logger.Printf("IDE distrubution logs path not found")
+
+	return "", nil
+}
+
+func exportDSYMs(dsymDir string, dsyms []string) error {
+	for _, dsym := range dsyms {
+		if err := v1command.CopyDir(dsym, dsymDir, false); err != nil {
+			return fmt.Errorf("could not copy (%s) to directory (%s): %s", dsym, dsymDir, err)
+		}
+	}
+	return nil
+}
